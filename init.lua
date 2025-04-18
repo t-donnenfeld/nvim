d3c195120f145749ef18752444521ee25c3e2d73
Theotime Donnenfeld <tdonnenfeld@proton.me>
2025-04-18 16:22:39 +0200

    update

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ vim.g.maplocalleader = ' '
 
 vim.opt.breakindent = true
 vim.opt.undofile = true
+vim.opt.autoindent = true
 
 -- Search
 vim.opt.ignorecase = true
@@ -35,6 +36,57 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+
+---- LSP CONFIGURATION -------
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('my.lsp', {}),
+  callback = function(args)
+    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+    if client:supports_method('textDocument/implementation') then
+      -- Create a keymap for vim.lsp.buf.implementation ...
+    end
+    -- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
+    if client:supports_method('textDocument/completion') then
+      -- Optional: trigger autocompletion on EVERY keypress. May be slow!
+      -- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+      -- client.server_capabilities.completionProvider.triggerCharacters = chars
+      vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+    end
+    -- Auto-format ("lint") on save.
+    -- Usually not needed if server supports "textDocument/willSaveWaitUntil".
+    if not client:supports_method('textDocument/willSaveWaitUntil')
+        and client:supports_method('textDocument/formatting') then
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
+        buffer = args.buf,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+        end,
+      })
+    end
+  end,
+})
+
+vim.lsp.config['luals'] = {
+  cmd = { 'lua-language-server' },
+  filetypes = { 'lua' },
+  root_markers = { '.luarc.json', '.luarc.jsonc' },
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+      }
+    }
+  }
+}
+vim.lsp.enable('luals')
+
+---- LSP CONFIGURATION -------
+
+
+
+
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
@@ -49,24 +101,67 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   'tpope/vim-sleuth',
   {
-      'sainnhe/gruvbox-material',
-      lazy = false,
-      priority = 1000,
-      config = function()
-        vim.g.gruvbox_material_enable_italic = true
-        vim.cmd.colorscheme('gruvbox-material')
-      end
+    'sainnhe/gruvbox-material',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.gruvbox_material_enable_italic = true
+      vim.cmd.colorscheme('gruvbox-material')
+    end
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local configs = require("nvim-treesitter.configs")
+      configs.setup({
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "json", "yaml" },
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end
+  },
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    opts = {}
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
     },
-    {
-     "nvim-treesitter/nvim-treesitter",
-     build = ":TSUpdate",
-     config = function () 
-       local configs = require("nvim-treesitter.configs")
-       configs.setup({
-           ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "json", "yaml"},
-           highlight = { enable = true },
-           indent = { enable = true },  
-         })
-     end
+    lazy = false, -- neo-tree will lazily load itself
+    ---@module "neo-tree"
+    ---@type neotree.Config?
+    opts = {
+      -- fill any relevant options here
+    },
+    keys = {
+      { "<leader>e", "<cmd>Neotree toggle<CR>", desc = "Open FileTree" }
     }
+  },
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    lazy = false,
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    keys = {
+      { '<leader><leader>', '<cmd>Telescope find_files<CR>', { desc = 'Telescope find files' } },
+      { '<leader>fg',       '<cmd>Telescope live_grep<CR>',  { desc = 'Telescope live grep' } },
+      { '<leader>fb',       '<cmd>Telescope buffers<CR>',    { desc = 'Telescope buffers' } },
+      { '<leader>fh',       '<cmd>Telescope help_tags<CR>',  { desc = 'Telescope help tags' } }
+    },
+    opts = {}
+  },
+  {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = true,
+    opts = {}
+  }
 })
